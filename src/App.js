e495c071daac46becd8108cd0149d871af696614
damzymichael <105460672+damzymichael@users.noreply.github.com>
2023-08-22 15:17:27 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import Home from "./pages/Home";
 import News from "./pages/News";
-import { useTheme } from "@emotion/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import Header from "./components/Header";
 
 function App() {
-  const theme = useTheme();
-  console.log(theme);
   return (
     <BrowserRouter>
       <ScrollToTop>
